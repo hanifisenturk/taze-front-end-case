70ff390e0fdfa2f9e0ce557807f7caccbce34d35
Mehmet Hanifi Senturk <mehmetsenturk@protonmail.com>
2022-07-20 13:03:01 +0300

    SearchCrypto component has been started to add.

---
+++ src/Components/SearchCrypto/SearchCrypto.jsx
@@ -0,0 +1,11 @@
+const SearchCrypto = () => {
+  return (
+    <input
+      className="py-2 px-4 border-none outline-none text-[1.8rem] rounded-md"
+      type="text"
+      placeholder="Search your crypto"
+    />
+  );
+};
+
+export default SearchCrypto;
