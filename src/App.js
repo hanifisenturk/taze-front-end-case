f3704c406b59371c9a1c52ee78a8e06879b7f60d
Mehmet Hanifi Senturk <mehmetsenturk@protonmail.com>
2022-07-20 19:47:59 +0300

    ContextApi's provider component has been added.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { ContextProvider } from "./store/context";
 import CryptoList from "./Components/CryptoList/CryptoList";
+import WatchList from "./Components/WatchList/WatchList";
 
 function App() {
   return (
-    <div className="bg-gradient-to-r from-[#23074d] to-[#cc5333]  min-h-screen bg-fixed py-10">
-      <CryptoList />
-    </div>
+    <ContextProvider>
+      <div className="bg-gradient-to-r from-[#23074d] to-[#cc5333]  min-h-screen bg-fixed py-10">
+        <WatchList />
+        <CryptoList />
+      </div>
+    </ContextProvider>
   );
 }
 
