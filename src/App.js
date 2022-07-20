60b31f81188b852d91d6302b76c445b75c6bb66b
Mehmet Hanifi Senturk <mehmetsenturk@protonmail.com>
2022-07-20 12:28:34 +0300

    CryptoList component has been added.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import CryptoList from "./Components/CryptoList/CryptoList";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="bg-gradient-to-r from-[#23074d] to-[#cc5333]  min-h-[400vh] py-10">
+      <CryptoList />
+    </div>
+  );
 }
 
 export default App;
