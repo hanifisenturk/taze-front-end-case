f7f34ddd3d5efa2cf2d91984b32390cb6bed76ee
Mehmet Hanifi Senturk <mehmetsenturk@protonmail.com>
2022-07-20 14:46:16 +0300

    Some CSS properties have been changed.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import CryptoList from "./Components/CryptoList/CryptoList";
 
 function App() {
   return (
-    <div className="bg-gradient-to-r from-[#23074d] to-[#cc5333]  min-h-[400vh] py-10">
+    <div className="bg-gradient-to-r from-[#23074d] to-[#cc5333]  min-h-screen py-10">
       <CryptoList />
     </div>
   );
