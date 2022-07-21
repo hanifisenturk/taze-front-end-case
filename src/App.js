d3135e86279caaee7349cdfe1d3903f0dc704b9d
Mehmet Hanifi Senturk <mehmetsenturk@protonmail.com>
2022-07-21 11:41:49 +0300

    Ugly bg-color has been changed.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Routes/Home";
+import NotFound from "./Routes/NotFound";
+import CoinInfo from "./Routes/CoinInfo";
 import { ContextProvider } from "./store/context";
-import CryptoList from "./Components/CryptoList/CryptoList";
-import WatchList from "./Components/WatchList/WatchList";
 
 function App() {
   return (
-    <ContextProvider>
-      <div className="bg-gradient-to-r from-[#23074d] to-[#cc5333]  min-h-screen bg-fixed py-10">
-        <WatchList />
-        <CryptoList />
-      </div>
-    </ContextProvider>
+    <BrowserRouter>
+      <ContextProvider>
+        <div className="bg-gradient-to-r from-[#F4D03F] to-[#16A085]  min-h-screen bg-fixed py-10">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/coin/:coinId" element={<CoinInfo />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </ContextProvider>
+    </BrowserRouter>
   );
 }
 
