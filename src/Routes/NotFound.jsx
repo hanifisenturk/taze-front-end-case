d287f2a4b3bf87227bc69f7594f017a996821403
Mehmet Hanifi Senturk <mehmetsenturk@protonmail.com>
2022-07-21 14:21:31 +0300

    404 page has been added.

---
+++ src/Routes/NotFound.jsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div className="h-screen bg-cover bg-[url('https://i.kym-cdn.com/photos/images/original/001/408/831/041.png')] bg-no-repeat flex items-center justify-center text-white text-[20rem]">
+      404 :(
+    </div>
+  );
+};
+
+export default NotFound;
