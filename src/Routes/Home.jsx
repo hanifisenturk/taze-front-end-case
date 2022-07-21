b6cb6a052f7090ae5687ca3865035af2f5df2ea6
Mehmet Hanifi Senturk <mehmetsenturk@protonmail.com>
2022-07-21 14:21:20 +0300

    Home page has been added.

---
+++ src/Routes/Home.jsx
@@ -0,0 +1,13 @@
+import CryptoList from "../Components/CryptoList/CryptoList";
+import WatchList from "../Components/WatchList/WatchList";
+
+const Home = () => {
+  return (
+    <>
+      <WatchList />
+      <CryptoList />
+    </>
+  );
+};
+
+export default Home;
